37698f1ea587fa49a388d15b4b822449d30be25e
Arash <94035184+01Arash1@users.noreply.github.com>
2025-09-02 02:05:12 +0330

    problem 293

--- 30.اعداد اول/solution.py
+++ 30.اعداد اول/solution.py
@@ -1,9 +1,16 @@
 a = int(input())
 b = int(input())
 
+start = min(a, b)
+end = max(a, b)
 
-while a <= b : 
-    for i in range(2 , a +1):
-        if a % i == 0:
-            print(a)
-    a += 1
+for num in range(start, end + 1):
+    if num < 2:
+        continue
+    is_prime = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
